daaf0a181f7b18ef483118840226e147dc9b7fd5
Dane Knight <knight1seven@gmail.com>
2017-08-22 15:58:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,5 +8,6 @@ module.exports = { name, height
 
 if (name === "Susan") {
   console.log("The name is correct")
-} else {"Expected the name to be Susan"
+} else {
+  ("Expected the name to be Susan")
 }
